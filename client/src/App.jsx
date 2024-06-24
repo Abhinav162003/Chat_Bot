2e9d1573edd0e213208b2d88a7fc8495d7ed0eb2
Abhinav162003 <ar0737@srmist.edu.in>
2024-06-24 23:10:44 +0530

    sucees

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {useState} from "react";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Chat from "@/components/chat";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+       </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
